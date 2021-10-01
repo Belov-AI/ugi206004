529023e70ce7c42412f9d998f3e1d52bb93b5652
Belov-AI <Belov.Alexander@urfu.ru>
2021-10-01 13:14:09 +0500

    Задание "Обобщенный метод"

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Max(new int[0]));
+            Console.WriteLine(Max(new[] { 3 }));
+            Console.WriteLine(Max(new[] { '+', '-', '*', '/' }));
+            Console.WriteLine(Max(new[] { 3, 1, 2 }));
+            Console.WriteLine(Max(new[] { "AAB", "ABA", "AAC" }));
+
+            Console.ReadKey();
+        }
+
+        static ... Max...(...[] source) ...
+        {
+            if(source.Length == 0)
+            return default(...);
+            ...
+        }
+    }
+}
