b675d3be431ec87769f5ac742a73400f298dde97
Belov-AI <Belov.Alexander@urfu.ru>
2021-10-15 13:05:19 +0500

    Добавлен метод расширения.

---
+++ ExtentionMethod/ExtentionMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rnd = new Random();
+
+            for(var i =0; i< 10; i++)
+                Console.WriteLine($"{rnd.NextDouble(1, 10):F3}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ ExtentionMethod/ExtentionMethod/RandomExtention.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtentionMethod
+{
+    public static class RandomExtention
+    {
+        public static double NextDouble(this Random rnd, double minValue, double maxValue)
+        {
+            if(minValue > maxValue)
+                throw new ArgumentOutOfRangeException("minValue не может быть больше maxValue");
+
+            return rnd.NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+}
