ba1831c9451c251f29be38a7857e94e90772b605
Belov-AI <Belov.Alexander@urfu.ru>
2021-10-15 11:12:50 +0500

    Добавлен проект Flags (пример реалиации шалона Template Method)

---
+++ Flags/Flags/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            var flags = new TwoColoredFlag[]
+            {
+                new UkraineFlag(),
+                new PolandFlag()
+            };
+            
+            foreach (var flag in flags)
+            {
+                flag.Draw();
+                Console.WriteLine();
+            }
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
